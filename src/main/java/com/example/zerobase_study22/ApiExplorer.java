4c088097043c891c42e42e5e76c289b35c095181
LIM <lim971230@gmail.com>
2022-11-09 16:13:35 +0900

    api 요청후 클래스에 저장

---
+++ src/main/java/com/example/zerobase_study22/ApiExplorer.java
@@ -0,0 +1,121 @@
+package com.example.zerobase_study22;
+
+import com.google.gson.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+import static java.net.URLEncoder.*;
+
+public class ApiExplorer {
+    public static String apiCall(int startIndex, int endIndex) throws IOException {
+
+        StringBuilder urlBuilder = new StringBuilder("http://openapi.seoul.go.kr:8088");    /*URL*/
+        urlBuilder.append("/" + encode("454c6d67686c696d3832466a5a476b", "UTF-8"));     /*인증키*/
+        urlBuilder.append("/" + encode("json", "UTF-8"));   /*요청파일타입*/
+        urlBuilder.append("/" + encode("TbPublicWifiInfo", "UTF-8"));   /*서비스명*/
+        urlBuilder.append("/" + encode(String.valueOf(startIndex), "UTF-8"));   /*요청시작위치*/
+        urlBuilder.append("/" + encode(String.valueOf(endIndex), "UTF-8"));   /*요청종료위치*/
+
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/xml");
+//        System.out.println("Response code: " + conn.getResponseCode()); /* 연결 자체에 대한 확인이 필요하므로 추가합니다.*/
+        BufferedReader rd;
+// 서비스코드가 정상이면 200~300사이의 숫자가 나옵니다.
+        if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+//        System.out.println(sb.toString());
+        return sb.toString();
+    }
+
+    public static void putJsonStringToClass(String res, ArrayList<WifiInfo> results) {
+        JsonObject tbPublicWifiInfo = JsonParser.parseString(res).getAsJsonObject().get("TbPublicWifiInfo").getAsJsonObject();
+        JsonArray rows = tbPublicWifiInfo.get("row").getAsJsonArray();
+
+        for (int i = 0; i < rows.size(); i++) {
+            JsonElement row = rows.get(i);
+            Gson gson = new Gson();
+            WifiInfo wifiInfo = gson.fromJson(row, WifiInfo.class);
+            results.add(wifiInfo);
+        }
+
+    }
+    public static ArrayList<WifiInfo> collectWifiInfos() throws IOException {
+        ArrayList<WifiInfo> results = new ArrayList<>();
+
+        // 와이파이 개수 가져오기
+        String jsonStr = apiCall(1, 1);
+        JsonObject tbPublicWifiInfo = JsonParser.parseString(jsonStr).getAsJsonObject().get("TbPublicWifiInfo").getAsJsonObject();
+        int wifiNum = tbPublicWifiInfo.get("list_total_count").getAsInt();
+        System.out.println(wifiNum);
+
+        int iterate1000Num = wifiNum / 1000;
+        int lastIterateNum = wifiNum % 1000;
+
+        // 1000개의 데이터씩 가져오기
+        int startIndex;
+        int endIndex;
+        for (int i = 0; i < iterate1000Num; i++) {
+            startIndex = (i) * 1000 + 1;
+            endIndex = (i) * 1000 + 1000;
+            jsonStr = apiCall(startIndex, endIndex);
+            putJsonStringToClass(jsonStr, results);
+        }
+
+        // 남은 데이터 가져오기
+        startIndex = iterate1000Num * 1000 + 1;
+        endIndex = startIndex + lastIterateNum - 1;
+        jsonStr = apiCall(startIndex, endIndex);
+        putJsonStringToClass(jsonStr, results);
+
+        return results;
+    }
+
+    public static void main(String[] args) {
+        String result;
+        try {
+//            result = apiCall(5);
+//            System.out.println(result);
+//
+//            JsonElement element = JsonParser.parseString(result);
+//            JsonObject object = element.getAsJsonObject();
+//
+//            JsonObject tbPublicWifiInfo = object.get("TbPublicWifiInfo").getAsJsonObject();
+//            int list_total_count = tbPublicWifiInfo.get("list_total_count").getAsInt();
+//            System.out.println("데이터 개수: " + list_total_count);
+//
+//            JsonObject res = tbPublicWifiInfo.get("RESULT").getAsJsonObject();
+//
+//            JsonArray rows = tbPublicWifiInfo.get("row").getAsJsonArray();
+//            for (int i = 0; i < rows.size(); i++) {
+//                JsonElement row = rows.get(i);
+//                Gson gson = new Gson();
+//                WifiInfo wifiInfo = gson.fromJson(row, WifiInfo.class);
+//                System.out.println(wifiInfo);
+//            }
+            ArrayList<WifiInfo> results = collectWifiInfos();
+            for (int i = 0; i < results.size(); i++) {
+                System.out.println(i + 1 + " : " + results.get(i));
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/com/example/zerobase_study22/WifiInfo.java
@@ -0,0 +1,62 @@
+package com.example.zerobase_study22;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class WifiInfo {
+    @SerializedName("X_SWIFI_MGR_NO")
+    private String mgrNo;  // 관리번호
+
+    private Double dist;
+
+    @SerializedName("X_SWIFI_WRDOFC")
+    private String wrdofc;  // 자치구
+
+    @SerializedName("X_SWIFI_MAIN_NM")
+    private String mainNm; // 와이파이명
+
+    @SerializedName("X_SWIFI_ADRES1")
+    private String adres1;  // 도로명주소
+
+    @SerializedName("X_SWIFI_ADRES2")
+    private String adres2;  // 상세 주소
+
+    @SerializedName("X_SWIFI_INSTL_FLOOR")
+    private String instlFloor; // 설치위치(층)
+
+    @SerializedName("X_SWIFI_INSTL_TY")
+    private String instlTy;    // 설치유형
+
+    @SerializedName("X_SWIFI_INSTL_MBY")
+    private String instlMby;   // 설치기관
+
+    @SerializedName("X_SWIFI_SVC_SE")
+    private String svcSe;  // 서비스 구분
+
+    @SerializedName("X_SWIFI_CMCWR")
+    private String cmcwr;   // 망종류
+
+    @SerializedName("X_SWIFI_CNSTC_YEAR")
+    private String cnstcYear;   // 설치년도
+
+    @SerializedName("X_SWIFI_INOUT_DOOR")
+    private String inoutDoor;   // 실내외구분
+
+    @SerializedName("X_SWIFI_REMARS3")
+    private String remars3; // wifi 접속환경
+
+    @SerializedName("LAT")
+    private String lat; // y좌표
+
+    @SerializedName("LNT")
+    private String lnt; // x좌표
+
+    @SerializedName("WORK_DTTM")
+    private String workDttm;    // 작업일자
+
+}
